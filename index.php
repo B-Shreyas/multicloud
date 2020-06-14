59a97e79f00ae2c62ee37df1ddb44df54d194a2a
Shreyas Basutkar <shreyasbasutkar21@gmail.com>
2020-06-14 23:50:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <body bgcolor = "aqua">
 Hello! Welcome to the Multi Hybrid Cloud !!!
-<img src = "https://terras3bucket.s3.ap-south-1.amazonaws.com/Shreyas.jpeg">  
+<img src = "https://terras3bucket.s3.ap-south-1.amazonaws.com/Shreyas.jpeg"    height="200" width="200">  
   
 <br/>
 
