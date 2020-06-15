3336f89826444a8c18710e150b71fd1230444782
Shreyas Basutkar <shreyasbasutkar21@gmail.com>
2020-06-15 23:23:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,17 @@
 <html>
-<body>
+<head>
 <body bgcolor='aqua'>
-<marquee><h1>After completing the task i was like !</h1></marquee>
+<h1>After completing the task i was like !</h1>
 
-<img src="https://shreyas_bucket97116685.s3.ap-south-1.amazonaws.com/Shreyas.jpg" alt="Girl in a jacket" width="200" height="200">
-
+<img src="https://mytaskbucketshreyas12.s3.ap-south-1.amazonaws.com/Shreyas.jpg" alt="Girl in a jacket" width="200" height="200">
+<pre>
+<?php
+$cloudant_url = `head -n1 path.txt`;
+$img_path="https://".$cloudant_url."/Shreyas.jpeg";
+echo "<br>";
+echo "<img src='${img_path}' width=100 height=100>";
+?>
+</pre>
 </body>
+</head>
 </html>
